a1fe6d897fe498fb13b1ea4d2cf6846c3ee0346b
zyl <me10zyl@qq.com>
2024-06-17 22:49:05 +0800

    .fix.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,31 +1,36 @@
 <script>
 import {defineComponent, reactive} from "vue";
 
-export defineComponent({
-  setup:
+export default defineComponent({
+  setup: (props, ctx) => {
+    return {
+
+    }
+  }
 })
 const scripts = reactive([]);
 </script>
 
 <template>
-<el-row style="margin: 0;padding: 0">
-  <el-col :span="8" class="left">
-    <ul>
-      <li v-for=""></li>
-    </ul>
-  </el-col>
-  <el-col :span="16" class="right">
+  <el-row style="margin: 0;padding: 0">
+    <el-col :span="8" class="left">
+      <ul>
+        <li v-for=""></li>
+      </ul>
+    </el-col>
+    <el-col :span="16" class="right">
 
-  </el-col>
-</el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <style scoped>
-.left{
+.left {
   background: #cccccc11;
   min-height: 500px;
 }
-.right{
-//border:  1px solid;
+
+.right {
+  //border:  1px solid;
 }
 </style>
